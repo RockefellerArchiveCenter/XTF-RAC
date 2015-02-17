784fcfa25d8aa43c15ba9db77faa87d2ba123b5c
helrond <helrond@hotmail.com>
2015-02-17 15:20:59 -0500

    expand container list toc by default

--- script/rac/accordion.js
+++ script/rac/accordion.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-$('.accordionButton').click(function() {
-$('.accordionButton').removeClass('on');
-$('.accordionContent').slideUp('normal');
-
-if($(this).next().is(':hidden')) {
-$(this).addClass('on');
-$(this).next().slideDown('normal');}
-});
-
-$('.accordionContent').hide();
-
+$(document).ready(function () {
+    $('.accordionButton').click(function () {
+        $('.accordionButton').removeClass('on');
+        $('.accordionContent').slideUp('normal');
+        
+        if ($(this).next().is(':hidden')) {
+            $(this).addClass('on');
+            $(this).next().slideDown('normal');
+        }
+    });
+    
+    $('.accordionContent').hide();
 });
